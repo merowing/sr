46284db9dd1b7f8d9a09a2d1a49e9f50f3a15855
Elgynart <elgynart@gmail.com>
2022-10-11 20:08:58 +0300

    test

--- scripts/index.js
+++ scripts/index.js
@@ -240,18 +240,9 @@ function initMap() {
     //     }
     // });
 
-    //geolocation(map);
+    geolocation(map);
 }
 
-function objToString (obj) {
-    var str = '';
-    for (var p in obj) {
-        if (Object.prototype.hasOwnProperty.call(obj, p)) {
-            str += p + '::' + obj[p] + '\n';
-        }
-    }
-    return str;
-}
 
 // window.initMap = initMap;
 

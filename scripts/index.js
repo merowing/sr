40a95dec4a424a94479b68a72e14ce6178919d85
Elgynart <elgynart@gmail.com>
2022-10-11 17:04:42 +0300

    test

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,14 @@
-import logo from './logo.js';
-import microphone from './micro.js';
+import logoIcon from './logo.js';
+import microphoneIcon from './micro.js';
 
 const text = document.querySelector('.text');
-const said = document.querySelector('.said');
+//const said = document.querySelector('.said');
 const voiceButton = document.querySelector('.voice-button');
-const smallvoiceButton = document.querySelector('.small-voice-button');
+//const smallvoiceButton = document.querySelector('.small-voice-button');
 const mapBlock = document.querySelector('#map');
 
-voiceButton.innerHTML = microphone;
-smallvoiceButton.innerHTML = microphone;
+voiceButton.innerHTML = microphoneIcon;
+//smallvoiceButton.innerHTML = microphone;
 
 let errorActive = false;
 
@@ -20,8 +20,7 @@ try {
     recognition.interimResults = false;
 
     let helpCommandsHtml = null;
-    function help() {
-        alert('help');
+    function helpCode() {
         if(helpCommandsHtml) return helpCommandsHtml;
 
         function inner() {
@@ -29,10 +28,6 @@ try {
                 {
                     name: 'logo, hello kottans',
                     description: 'show logo',
-                },
-                {
-                    name: 'stop',
-                    description: 'stop recording voice',
                 },
                 {
                     name: 'close',
@@ -65,7 +60,7 @@ try {
             }, '');
 
             return `
-                <h4>List of commands</h4>
+                <h3>List of commands</h3>
                 <ul>
                     ${list}
                 </ul>
@@ -92,24 +87,23 @@ try {
                 command = 'hello kottans';
             }
 
+            //text.classList.remove('help');
+
             switch(command) {
                 case 'close':
-                    text.innerHTML = helloScreen();
-                    break;
-
-                case 'stop':
-                    stop();
+                    close();
                     break;
 
                 case 'help':
                 case 'info':
-                    text.classList.add('help');
-                    text.innerHTML = help();
+                    //text.classList.add('help');
+                    //text.innerHTML = help();
+                    help();
                     break;
 
                 case 'logo':
                 case 'hello kottans':
-                    text.innerHTML = logo;
+                    logo();
                     break;
 
                 case 'my location':
@@ -123,13 +117,14 @@ try {
                     break;
 
                 case 'github':
-                    window.open('https://github.com/merowing', '_blank');
+                    //window.open('https://github.com/merowing', '_blank');
+                    github();
                     break;
 
                 default:
-                    voiceButton.classList.remove('hidden');
-                    smallvoiceButton.classList.add('hidden');
-                    text.innerHTML = unrecognized(str);
+                    //voiceButton.classList.remove('hidden');
+                    //smallvoiceButton.classList.add('hidden');
+                    unrecognized(str);
             }
         }
     });
@@ -164,16 +159,29 @@ try {
     });
 
     function close() {
-        text.innerHTML = '';
-        mapBlock.classList.add('hidden');
         text.classList.remove('hidden');
-        smallvoiceButton.classList.remove('hidden');
-        voiceButton.classList.add('hidden');
+        text.innerHTML = helloScreen();
+    }
+
+    function logo() {
+        text.classList.remove('hello');
+        text.innerHTML = logoIcon;
+    }
+
+    function github() {
+        text.classList.remove('hello');
+        text.innerHTML = '<iframe name="github" src="https://github.com/merowing"></iframe>'
+    }
+
+    function help() {
+        text.classList.remove('hello');
+        text.innerHTML = helpCode();
     }
 
     function helloScreen() {
         //voiceButton.classList.remove('hidden');
         //smallvoiceButton.classList.add('hidden');
+        text.classList.add('hello');
         return `<h3>Hi!</h3>Tap the microphone icon and<br> say <strong>'Help'</strong> to show commands.`;
     }
     text.innerHTML = helloScreen();
@@ -181,6 +189,7 @@ try {
     function stop() {
         //text.classList.remove('hidden');
         voiceButton.classList.remove('active');
+        //text.classList.add('hello');
         //voiceButton.classList.remove('hidden');
         //smallvoiceButton.classList.add('hidden');
         //mapBlock.classList.add('hidden');
@@ -194,16 +203,17 @@ try {
     }
 
     function unrecognized(str) {
-        return `
-                <span style="color: red;">Unrecognized command.</span>
-                <br>
-                <span>
-                    <strong>${str}</strong>
-                </span>
-                <br>
-                <br>
-                <span>Say <strong>'Help'</strong> to show available commands</span>
-            `;
+        text.classList.add('hello');
+        text.innerHTML = `
+                    <span style="color: red;">Unrecognized command.</span>
+                    <br>
+                    <span>
+                        <strong>${str}</strong>
+                    </span>
+                    <br>
+                    <br>
+                    <span>Say <strong>'Help'</strong> to show available commands</span>
+                `;
     }
 
     function transcript(str) {
@@ -219,11 +229,13 @@ try {
 }
 
 function initMap() {
-    mapBlock.classList.remove('hidden');
-    text.classList.add('hidden');
+    text.innerHTML = `<div id="map"></div>`;
+    text.classList.remove('hello');
+    //mapBlock.classList.remove('hidden');
+    //text.classList.add('hidden');
     //smallvoiceButton.classList.remove('hidden');
 
-    const map = new google.maps.Map(mapBlock, {
+    const map = new google.maps.Map(document.querySelector('#map'), {
         center: { lat: -34.397, lng: 150.644 },
         zoom: 6,
         mapTypeControl: false,
